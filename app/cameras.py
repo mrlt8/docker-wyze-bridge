c4beb2b39e189fcac872a9f45e1fbea774c91a56
mrlt8 <67088095+mrlt8@users.noreply.github.com>
2021-06-28 15:03:03 +0900

    Restart on failed login or ffmpeg error
    Restart wyzecam-to-rtmp.py if `requests.exceptions.HTTPError` during login or `Conversion failed!` in  ffmpeg.

--- app/cameras.py
+++ app/cameras.py
@@ -1,13 +1,31 @@
 from wyzecam import login, get_camera_list
-import os,subprocess,concurrent.futures
+import os,subprocess,threading,time
 
 auth_info = login(os.environ["WYZE_EMAIL"], os.environ["WYZE_PASSWORD"])
 def rtsp_server(name):
     while True:
-        print(f'starting rtsp server for {name}...')
+        print(f'[{name}]: Starting...',flush=True)
         os.environ["WYZE_CAMERA_NAME"] = name
         os.environ["RTMP_URL"] = 'rtmp://rtsp-server:1935/' + name.replace(' ', '-').lower()
-        subprocess.call(['python', '/opt/wyzecam/wyzecam-to-rtmp.py']).wait()
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    for camera in get_camera_list(auth_info):
-        executor.submit(rtsp_server, camera.nickname)
+        ffmpeg = subprocess.Popen(['python', '/opt/wyzecam/wyzecam-to-rtmp.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+        while True:
+            output = ffmpeg.stdout.readline().rstrip()
+            if not output:
+                break
+            if 'DEBUG_FFMPEG' in os.environ:
+                print(f'[{name}]: {output.rstrip()}')
+                continue
+            if 'requests.exceptions.HTTPError' in output:
+                print(f'[{name}]: {output.rstrip()}',flush=True)
+                ffmpeg.kill()
+                break
+            if os.environ["RTMP_URL"] in output:
+                print(f'[{name}]: {output.rstrip()}',flush=True)
+            if 'Conversion failed!' in output:
+                print(f'[{name}]: {output.rstrip()}',flush=True)
+                ffmpeg.kill()
+                break
+        time.sleep(10)
+
+for camera in get_camera_list(auth_info):
+    threading.Thread(target=rtsp_server, args=(camera.nickname,)).start()
