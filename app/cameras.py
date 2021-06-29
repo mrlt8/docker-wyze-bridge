60d5be0350c920b41c301e409eb1f44ca5802a3d
mrlt8 <67088095+mrlt8@users.noreply.github.com>
2021-06-29 13:22:53 +0900

    Update cameras.py

--- app/cameras.py
+++ app/cameras.py
@@ -1,28 +1,28 @@
 from wyzecam import login, get_camera_list
-import os,subprocess,threading,time
+from subprocess import Popen, PIPE, STDOUT
+from threading import Thread
+from os import environ
+from time import sleep
 
-auth_info = login(os.environ["WYZE_EMAIL"], os.environ["WYZE_PASSWORD"])
+auth_info = login(environ["WYZE_EMAIL"], environ["WYZE_PASSWORD"])
+
 def rtsp_server(name):
     while True:
         print(f'[{name}]: Starting...',flush=True)
-        os.environ["WYZE_CAMERA_NAME"] = name
-        os.environ["RTMP_URL"] = 'rtmp://rtsp-server:1935/' + name.replace(' ', '-').lower()
-        ffmpeg = subprocess.Popen(['python', '/opt/wyzecam/wyzecam-to-rtmp.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-        while True:
+        environ["WYZE_CAMERA_NAME"] = name
+        environ["RTMP_URL"] = 'rtmp://rtsp-server:1935/' + name.replace(' ', '-').lower()
+        ffmpeg = Popen(['python', '/opt/wyzecam/wyzecam-to-rtmp.py'], stdout=PIPE,stderr=STDOUT, text=True)
+        while ffmpeg.poll() is None:
             output = ffmpeg.stdout.readline().rstrip()
             if not output:
-                break
-            if 'DEBUG_FFMPEG' in os.environ:
-                print(f'[{name}]: {output.rstrip()}')
                 continue
-            if os.environ["RTMP_URL"] in output:
+            if 'DEBUG_FFMPEG' in environ or any(err in output for err in [environ["RTMP_URL"], 'requests.exceptions.HTTPError','Conversion failed!','Broken pipe']):
                 print(f'[{name}]: {output.rstrip()}',flush=True)
-                continue
-            if ('requests.exceptions.HTTPError' in output) or ('Conversion failed!' in output):
-                print(f'[{name}]: {output.rstrip()}',flush=True)
-                ffmpeg.kill()
+            if any(err in output for err in ['requests.exceptions.HTTPError','Broken pipe','Conversion failed!']):
                 break
-        time.sleep(10)
+        print(f'[{name}]: Killing ffmpeg...',flush=True)
+        ffmpeg.kill()
+        sleep(1)
 
 for camera in get_camera_list(auth_info):
-    threading.Thread(target=rtsp_server, args=(camera.nickname,)).start()
+    Thread(target=rtsp_server, args=(camera.nickname,)).start()
