95580379d28b63d7e5765276fdb483e66000c501
mrlt8 <67088095+mrlt8@users.noreply.github.com>
2022-04-26 19:02:38 -0700

    Verbose frame/bitrate change #347

--- app/wyzecam/iotc.py
+++ app/wyzecam/iotc.py
@@ -493,7 +493,7 @@ class WyzeIOTCSession:
                 else:
                     iotc_msg = False
             if iotc_msg:
-                logger.info("Requesting frame_size=%d and bitrate=%d" % iotc_msg)
+                logger.warn("Requesting frame_size=%d and bitrate=%d" % iotc_msg)
                 try:
                     if self.camera.product_model in ("WYZEDB3", "WVOD1"):
                         mux.send_ioctl(K10052DBSetResolvingBit(*iotc_msg)).result(False)
@@ -512,7 +512,7 @@ class WyzeIOTCSession:
 
     def change_fps(self, fps: int) -> None:
         """Send a message to the camera to update the FPS."""
-        logger.info("Requesting frame_rate=%d" % fps)
+        logger.warn("Requesting frame_rate=%d" % fps)
         with self.iotctrl_mux() as mux:
             try:
                 mux.send_ioctl(K10052DBSetResolvingBit(0, 0, fps)).result(block=False)
