36b6fc4fc3f7e28c59039f162d25ad393eb353f3
mrlt8 <67088095+mrlt8@users.noreply.github.com>
2022-10-31 07:43:16 -0700

    Add img_time to API

--- app/wyze_bridge.py
+++ app/wyze_bridge.py
@@ -602,6 +602,11 @@ class WyzeBridge:
             hostname = self.hostname
 
         img = f"{name_uri}.{env_bool('IMG_TYPE','jpg')}"
+        try:
+            img_time = round(os.path.getmtime(self.img_path + img))
+        except FileNotFoundError:
+            img_time = None
+
         data = {
             "nickname": cam.nickname,
             "status": self.get_cam_status(name_uri),
@@ -623,7 +628,8 @@ class WyzeBridge:
             "enabled": name_uri in self.streams,
             "camera_info": None,
             "boa_url": None,
-            "img_url": f"img/{img}" if os.path.exists(self.img_path + img) else None,
+            "img_url": f"img/{img}" if img_time else None,
+            "img_time": img_time,
             "snapshot_url": f"snapshot/{img}",
             "photo_url": None,
         }
@@ -631,7 +637,7 @@ class WyzeBridge:
             data["hls_url"] = data["hls_url"].replace("http:", "https:")
         if stream := self.streams.get(name_uri):
             if stream.get("stop_flag") and not stream["stop_flag"].is_set():
-                data["started"] = int(stream.get("started", 0) * 1000)
+                data["started"] = round(stream.get("started", 0))
             if stream.get("camera_info"):
                 data["connected"] = True
                 data["camera_info"] = stream["camera_info"]
