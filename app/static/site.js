720bf263fdece2171e7b8e37cb8b4d1675a1a387
mrlt8 <67088095+mrlt8@users.noreply.github.com>
2023-06-04 23:45:01 +0900

    autoplay and WebUI related fixes (#828)
    * Autoplay on fullscreen or autoplay url param #826
    
    * fix TypeError
    
    * Update index.html
    
    * dim fullscreen mode card-header-title
    
    * Remove WebRTC if not enabled
    
    * version bump

--- app/static/site.js
+++ app/static/site.js
@@ -240,7 +240,7 @@ function refresh_imgs() {
   console.debug("refresh_imgs " + Date.now());
   document.querySelectorAll(".refresh_img").forEach(async function (image) {
     let url = image.getAttribute("src");
-    // Skip if not enabled or battery
+    if (url === null) { return; }
     let CameraBattery = document.getElementById(image.dataset.cam).dataset.battery?.toLowerCase() == "true";
     let CameraConnected = image.classList.contains("connected");
     let CameraEnabled = image.classList.contains("enabled");
@@ -578,9 +578,11 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".fs-display-none").forEach((e) => {
       if (fs) { e.classList.add("fs-mode") } else { e.classList.remove("fs-mode") }
     })
+    if (fs) { autoplay(); }
   }
+
   document.querySelector(".fullscreen button").addEventListener("click", () => {
-    let fs = !getCookie("fullscreen", false) ? "1" : "";
+    let fs = getCookie("fullscreen", false) ? "" : "1";
     setCookie("fullscreen", fs)
     toggleFullscreen(fs)
   })
@@ -612,9 +614,10 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
     let autoPlay = getCookie("autoplay");
+    let fullscreen = getCookie("fullscreen");
     videos.forEach(video => {
       video.classList.remove("lost");
-      if (!autoPlay) { return }
+      if (!autoPlay && !fullscreen && !video.autoplay) { return }
       if (video.classList.contains("webrtc")) {
         loadWebRTC(video);
       } else if (video.classList.contains("vjs-tech")) {
