15659b935bdacb25d19cb3c237073d96da48fdd7
mrlt8 <67088095+mrlt8@users.noreply.github.com>
2023-12-23 18:39:38 -0700

    use genpts #388

--- app/wyzebridge/ffmpeg.py
+++ app/wyzebridge/ffmpeg.py
@@ -29,7 +29,7 @@ def get_ffmpeg_cmd(
     - list of str: complete ffmpeg command that is ready to run as subprocess.
     """
 
-    flags = "-fflags +flush_packets+nobuffer -flags +low_delay"
+    flags = "-fflags +flush_packets+nobuffer+genpts -flags +low_delay"
     livestream = get_livestream_cmd(uri)
     audio_in = "-f lavfi -i anullsrc=cl=mono" if livestream else ""
     audio_out = "aac"
@@ -60,7 +60,7 @@ def get_ffmpeg_cmd(
         + re_encode_video(uri, is_vertical)
         + (["-map", "1:a", "-c:a", audio_out] if audio_in else [])
         + (a_options if audio and audio_out != "copy" else [])
-        + ["-fps_mode", "cfr", "-async", "0"]
+        + ["-max_muxing_queue_size", "256", "-max_delay", "50", "-rtbufsize", "256k"]
         + ["-movflags", "frag_keyframe+empty_moov"]
         + ["-f", "tee"]
         + [rtsp_ss + get_record_cmd(uri, audio_out, record) + livestream]
