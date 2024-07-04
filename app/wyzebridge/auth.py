0ffe0d68ebd39fc9b308267d4d84f525380d9275
mrlt8 <67088095+mrlt8@users.noreply.github.com>
2024-07-03 20:23:00 -0700

    refactor WebUI credentials

--- app/wyzebridge/auth.py
+++ app/wyzebridge/auth.py
@@ -19,8 +19,8 @@ def get_secret(name: str, default: str = "") -> str:
         return env_bool(name, default, style="original")
 
 
-def get_password(file_name: str, alt: str = "") -> str:
-    if env_pass := get_secret(file_name, get_secret(alt)):
+def get_credential(file_name: str) -> str:
+    if env_pass := get_secret(file_name):
         return env_pass
 
     file_path = f"{TOKEN_PATH}{file_name}"
@@ -31,7 +31,7 @@ def get_password(file_name: str, alt: str = "") -> str:
     return ""
 
 
-def clear_local_auth():
+def clear_local_creds():
     for file in ["wb_password", "wb_api"]:
         file_path = f"{TOKEN_PATH}{file}"
         if os.path.exists(file_path):
@@ -47,15 +47,17 @@ def gen_api_key(email):
 class WbAuth:
     enabled: bool = bool(env_bool("WB_AUTH") if os.getenv("WB_AUTH") else True)
     username: str = get_secret("wb_username", "wbadmin")
-    password: str = get_password("wb_password")
-    api: str = get_password("wb_api")
-    _hashed_password: Optional[str] = None
+    api: str = get_credential("wb_api")
+    _pass: str = get_credential("wb_password")
+    _hashed_pass: Optional[str] = None
 
     @classmethod
     def hashed_password(cls) -> str:
-        if cls._hashed_password:
-            return cls._hashed_password
-        return generate_password_hash(cls.password)
+        if cls._hashed_pass:
+            return cls._hashed_pass
+
+        cls._hashed_pass = generate_password_hash(cls._pass)
+        return cls._hashed_pass
 
     @classmethod
     def set_email(cls, email: str, force: bool = False):
@@ -63,24 +65,22 @@ class WbAuth:
         if not cls.enabled:
             return
 
-        if forced := (force or env_bool("FRESH_DATA")):
-            clear_local_auth()
-
-        cls._set_password(email, forced)
-        cls._set_api(email, forced)
+        cls._update_credentials(email, force)
 
         logger.info(f"[AUTH] WB_USERNAME={cls.username}")
-        logger.info(f"[AUTH] WB_PASSWORD={cls.password[0]}{'*'*(len(cls.password)-1)}")
+        logger.info(f"[AUTH] WB_PASSWORD={cls._pass[0]}{'*'*(len(cls._pass)-1)}")
         logger.info(f"[AUTH] WB_API={cls.api}")
 
     @classmethod
-    def _set_password(cls, email: str, forced: bool = False) -> None:
-        if forced or not cls.password:
-            cls.password = email.partition("@")[0]
+    def _update_credentials(cls, email: str, force: bool = False) -> None:
+        if force or env_bool("FRESH_DATA"):
+            clear_local_creds()
 
-    @classmethod
-    def _set_api(cls, email: str, forced: bool = False) -> None:
-        if forced or not cls.api:
+        if not get_credential("wb_password"):
+            cls._pass = email.partition("@")[0]
+            cls._hashed_pass = generate_password_hash(cls._pass)
+
+        if not get_credential("wb_api"):
             cls.api = gen_api_key(email)
 
 
